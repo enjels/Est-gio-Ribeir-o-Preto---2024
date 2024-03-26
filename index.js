7394a4d1e8faadb82fdc8ecd20f62f682065fbb4
Virgílio César <enjels@outlook.com>
2024-03-26 09:06:51 -0300

    solução do primeiro problema resolvido com while

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ Ao final do processamento, qual será o valor da variável SOMA? */
 
 let INDICE = 13, SOMA = 0, K = 0;
 
-
+while (K < INDICE) {
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+console.log(SOMA);
 
 
 
